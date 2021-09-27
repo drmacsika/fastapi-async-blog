80d1767e028b418c0d4fc926cd79ba209634e3f5
drmacsika <drmacsika@gmail.com>
2021-09-28 00:37:50 +0100

    Reorganise the main app to fit in async database operations.

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-
 from fastapi import Depends, FastAPI
 
-# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-# from tortoise.contrib.fastapi import register_tortoise
 from blog import main as blog_router
 
 app = FastAPI()
 
 app.include_router(blog_router.router, tags=["blog"])
 
-
-
-
-
-
 @app.get("/", tags=["home"])
 async def home():
     return {"Hello": "World"}
 
-# register_tortoise(
-#     app,
-#     db_url="sqlite://db.sqlite3",
-#     modules={'models': ["blog.models", "accounts.models"]},
-#     generate_schemas=True,
-#     add_exception_handlers=True
-# )
