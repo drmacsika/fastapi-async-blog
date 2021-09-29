00a7240bfb9ab9fe4f7f0c4c5ec371b0525cd635
drmacsika <drmacsika@gmail.com>
2021-09-29 23:16:59 +0100

    Add models migrations.

--- migrations/env.py
+++ migrations/env.py
@@ -27,12 +27,6 @@ target_metadata = Base.metadata # Here
 # ... etc.
 
 
-# def get_url():
-#     user = os.getenv("POSTGRES_USER", "postgres")
-#     password = os.getenv("POSTGRES_PASSWORD", "")
-#     server = os.getenv("POSTGRES_SERVER", "db")
-#     db = os.getenv("POSTGRES_DB", "app")
-#     return f"postgresql://{user}:{password}@{server}/{db}"
 
 
 def run_migrations_offline():
@@ -90,3 +84,46 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     asyncio.run(run_migrations_online())
+
+
+"""
+def get_url():
+    user = os.getenv("POSTGRES_USER", "postgres")
+    password = os.getenv("POSTGRES_PASSWORD", "")
+    server = os.getenv("POSTGRES_SERVER", "db")
+    db = os.getenv("POSTGRES_DB", "app")
+    return f"postgresql://{user}:{password}@{server}/{db}"
+
+
+def run_migrations_offline():
+    url = get_url()
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True, compare_type=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+  
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
+    connectable = engine_from_config(
+        configuration, prefix="sqlalchemy.", poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata, compare_type=True
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
+"""
