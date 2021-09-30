bd191301c89719c419e6d7791a022a1e312a11fa
drmacsika <drmacsika@gmail.com>
2021-09-30 22:39:08 +0100

    Add schemas to accounts.

---
+++ accounts/schemas.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+
+from blog.models import Post
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    name: str
+    username: str
+    email: EmailStr
+    
+
+class UserCreate(UserBase):
+    password: str
+    
+    
+class UserUpdate(BaseModel):
+    name: str
+    username: str
+    active: bool = False
+
+
+class User(UserBase):
+    id: int
+    articles: List[Post] = []
+    staff: bool = False
+    admin: bool = False
+    active: bool = False
+    
+    class Config:
+        orm_mode = True
+
+
+class UserInDB(User):
+    password: str
