c56908bd5c2dc777da9c9f0ab0b021acf96d3757
drmacsika <drmacsika@gmail.com>
2021-10-01 23:59:54 +0100

    Add length to string

--- blog/models.py
+++ blog/models.py
@@ -2,35 +2,41 @@ from core.settings import settings
 from sqlalchemy import (Boolean, Column, DateTime, ForeignKey, Integer, String,
                         Text)
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 Base = settings.Base
 
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    author = relationship("User", back_populates="articles")
     author_id = Column(Integer, ForeignKey("users.id"))
-    categories = relationship("Category", back_populates="post")
-    title = Column(String(length=100), index=True)
+    title = Column(String(length=100), index=True, nullable=False)
     description = Column(String(length=250), nullable=True)
     intro = Column(String(length=200), nullable=True)
     content = Column(Text)
-    slug = Column(String)
+    slug = Column(String(length=255), nullable=False, unique=True)
     read_length = Column(Integer, default=0)
     view_count = Column(Integer, default=0)
     active = Column(Boolean, default=False)
-    created = Column(DateTime)
-    updated = Column(DateTime)
+    created = Column(DateTime, server_default=func.now())
+    updated = Column(DateTime, onupdate=func.now())
+    
+    # Relationships
+    author = relationship("User", back_populates="articles")
+    categories = relationship("Category", back_populates="post")
+
 
 
 class Category(Base):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     post_id = Column(Integer, ForeignKey("posts.id"))
-    post = relationship("Post", back_populates="categories")
     title = Column(String(length=100), index=True)
     description = Column(String(length=250), nullable=True)
-    slug = Column(String)
+    slug = Column(String(length=255), nullable=False, unique=True)
     active = Column(Boolean, default=False)
     created = Column(DateTime)
     updated = Column(DateTime)
+    
+    # Relationships
+    post = relationship("Post", back_populates="categories")
