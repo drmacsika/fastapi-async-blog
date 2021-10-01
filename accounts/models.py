f5fbfd2db4733b4048de89bfd12b4ab10e33a8b8
drmacsika <drmacsika@gmail.com>
2021-10-01 23:54:17 +0100

    Add default values for created and updated columns

--- accounts/models.py
+++ accounts/models.py
@@ -1,22 +1,28 @@
 from core.settings import settings
 from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 Base = settings.Base
 
 class User(Base):
+    """Account details for authors, editors, and contributors"""
+    
     __tablename__ = "users"
+    
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, nullable=False)
     username = Column(String, nullable=False, unique=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    articles = relationship("Post", back_populates="author")
     password = Column(String, nullable=False)
     active = Column(Boolean, default=False)
     staff = Column(Boolean, default=False)
     admin = Column(Boolean, default=False)
-    created = Column(DateTime)
-    updated = Column(DateTime)
+    created = Column(DateTime, server_default=func.now())
+    updated = Column(DateTime, onupdate=func.now())
+    
+    # Relationships
+    articles = relationship("Post", back_populates="author")
     
     # posts = relationship(
     #     "Recipe",
