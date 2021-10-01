cab8abda7f66d4f68b76db5b2bf693829b482f8e
drmacsika <drmacsika@gmail.com>
2021-10-01 23:51:51 +0100

    Add default values for created and updated columns

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,19 @@
 from core.settings import settings
 from sqlalchemy import Boolean, Column, DateTime, Integer, String, Text
+from sqlalchemy.sql import func
 
 Base = settings.Base
 
 class Contact(Base):
+    """Get contact messages from visitors"""
+    
     __tablename__ = "contacts"
+    
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    firstname = Column(String, nullable=False)
-    lastname = Column(String, nullable=False)
-    email = Column(String, index=True, nullable=False)
+    firstname = Column(String(100), nullable=False)
+    lastname = Column(String(100), nullable=False)
+    email = Column(String(255), index=True, nullable=False)
     message = Column(Text, nullable=False)
     status = Column(Boolean, default=True)
-    created = Column(DateTime)
+    created = Column(DateTime, server_default=func.now())
     
