9aec27a71c06bf4145f487189fd4bfe8e7850c67
drmacsika <drmacsika@gmail.com>
2021-10-03 23:54:13 +0100

    Add random string generator.

--- core/dependencies.py
+++ core/dependencies.py
@@ -1,5 +1,6 @@
 import base64
 import random
+import string
 from datetime import datetime
 from functools import lru_cache
 
@@ -9,7 +10,7 @@ from fastapi import Depends
 from core.settings import settings
 
 
-def generate_hotp(email: str, settings: Settings = Depends(settings)):
+def generate_hotp(email: str, settings: settings = Depends(settings)):
     keygen = email+str(datetime.date(datetime.now()))+settings.SECRET_KEY
     key = base64.b32encode(keygen.encode())
     return pyotp.HOTP(key)
@@ -23,3 +24,9 @@ def create_otp(email: str, counter: int):
 def verify_otp(email: str, token: str, counter: int):
     hotp = generate_hotp(email)
     return hotp.verify(token, counter) # => True
+
+
+def random_string(size: int, chars: str = string.ascii_lowercase+string.digits) -> str:
+    return "".join(random.choices(chars, k = size))
+
+
