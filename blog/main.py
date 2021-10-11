2a321a39d4bbda5da70c2d382faea58df27024fe
drmacsika <drmacsika@gmail.com>
2021-10-11 01:44:32 +0100

    Add endpoints for Post Create, Delete, Update

--- blog/main.py
+++ blog/main.py
@@ -9,13 +9,39 @@ from sqlalchemy.orm import Session
 from blog.crud import (delete_item, get_item, get_multiple_items, post_item,
                        update_item)
 from blog.models import Category, Post
-from blog.schemas import CategoryOut, CreateCategory, PostOut, UpdateCategory
+from blog.schemas import (CategoryOut, CreateCategory, CreatePost, PostOut,
+                          UpdateCategory, UpdatePost)
 
 router = APIRouter()
+
+@router.post("/blog/create/", status_code=201, response_model = PostOut, tags=["Blog Post"])
+async def create_post(request: CreatePost, db: AsyncSession = Depends(get_session)):
+    return await post_item(item=request, db=db, cls=Post)
 
 @router.get("/blog/{slug}/", response_model=PostOut, tags=["Blog Post"])
 async def get_post(slug: str, db: AsyncSession = Depends(get_session)):
     return get_item(slug=slug, cls=Post, db=db)
+
+
+@router.get("/blog/", response_model=List[PostOut], tags=["Blog Post"])
+async def get_all_posts(db: AsyncSession = Depends(get_session)):
+    return await get_multiple_items(cls=Post, db=db)
+
+
+@router.put("/blog/{slug}/update/", status_code=200, response_model=PostOut, tags=["Blog Post"])
+async def update_post(request: UpdatePost, slug: str, db: AsyncSession = Depends(get_session)):
+    return await update_item(item=request, slug=slug, db=db, cls=Post)
+
+
+@router.delete("/blog/{slug}/delete/", status_code=410, response_model=PostOut, tags=["Blog Post"])
+async def delete_post(request: UpdatePost, slug: str, db: AsyncSession):
+    return await delete_item(item=request, slug=slug, cls=Post, db=db)
+
+
+@router.post("/blog/tag/create/", status_code=201, response_model=CategoryOut, tags=["Blog Category"])
+async def create_category(request: CreateCategory, db: AsyncSession = Depends(get_session)):
+    return await post_item(item=request, cls=Category, db=db)
+
 
 @router.get("/blog/tag/{slug}/", response_model=CategoryOut, tags=["Blog Category"])
 async def get_category(slug: str, db: AsyncSession = Depends(get_session)):
@@ -27,17 +53,12 @@ async def get_all_categories(db: AsyncSession = Depends(get_session)):
     return await get_multiple_items(cls=Category, db=db)
 
 
-@router.post("/blog/tag/create/", status_code=201, response_model=CategoryOut, tags=["Blog Category"])
-async def create_category(request: CreateCategory, db: AsyncSession = Depends(get_session)):
-    return await post_item(item=request, cls=Category, db=db)
-
-
-@router.put("/blog/tag/update/{slug}/", status_code=200, response_model=CategoryOut, tags=["Blog Category"])
+@router.put("/blog/tag/{slug}/update/", status_code=200, response_model=CategoryOut, tags=["Blog Category"])
 async def update_category(request: UpdateCategory, slug: str, db: AsyncSession = Depends(get_session)):
     return await update_item(item=request, slug=slug, db=db, cls=Category)
     
 
-@router.delete("/blog/tag/delete/{slug}/", status_code=410, tags=["Blog Category"])
+@router.delete("/blog/tag/{slug}/delete/", status_code=410, tags=["Blog Category"])
 async def delete_category(request: UpdateCategory, slug: str, db: AsyncSession = Depends(get_session), tags=["Blog Category"]):
     return await delete_item(item=request, slug=slug, cls=Category, db=db)
     
