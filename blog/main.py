7486b5f594e775e527bfb5190c2d43130dddff81
drmacsika <drmacsika@gmail.com>
2021-10-13 20:30:06 +0100

    Removed the tags showing bug.

--- blog/main.py
+++ blog/main.py
@@ -12,19 +12,6 @@ from blog.schemas import (CategoryOut, CreateCategory, CreatePost, PostOut,
 
 router = APIRouter()
 
-@router.post("/blog/create/", status_code=201, response_model = PostOut, tags=["Blog Post"])
-async def create_post(request: CreatePost, db: AsyncSession = Depends(get_session)):
-    """
-    End point for blog post creation. This should always be placed above
-    the blog single GET endpoint.
-    """
-    return await post_item(item=request, db=db, cls=Post)
-
-@router.get("/blog/{slug}/", response_model=PostOut, tags=["Blog Post"])
-async def get_post(slug: str, db: AsyncSession = Depends(get_session)):
-    """Endpoint to get single blog post."""
-    return get_item(slug=slug, cls=Post, db=db)
-
 
 @router.get("/blog/", response_model=List[PostOut], tags=["Blog Post"])
 async def get_all_posts(db: AsyncSession = Depends(get_session)):
@@ -32,16 +19,20 @@ async def get_all_posts(db: AsyncSession = Depends(get_session)):
     return await get_multiple_items(cls=Post, db=db)
 
 
-@router.put("/blog/{slug}/update/", status_code=200, response_model=PostOut, tags=["Blog Post"])
-async def update_post(request: UpdatePost, slug: str, db: AsyncSession = Depends(get_session)):
-    """Endpoint to update blog post."""
-    return await update_item(item=request, slug=slug, db=db, cls=Post)
+@router.get("/blog/tags/", response_model=List[CategoryOut], tags=["Blog Category"])
+async def get_all_categories(db: AsyncSession = Depends(get_session)):
+    """Endpoint to get all blog posts or filtered blog posts."""
+    return await get_multiple_items(cls=Category, db=db)
 
 
-@router.delete("/blog/{slug}/delete/", status_code=410, response_model=PostOut, tags=["Blog Post"])
-async def delete_post(request: UpdatePost, slug: str, db: AsyncSession):
-    """Endpoint to delete blog post."""
-    return await delete_item(item=request, slug=slug, cls=Post, db=db)
+
+@router.post("/blog/create/", status_code=201, response_model = PostOut, tags=["Blog Post"])
+async def create_post(request: CreatePost, db: AsyncSession = Depends(get_session)):
+    """
+    End point for blog post creation. This should always be placed above
+    the blog single GET endpoint.
+    """
+    return await post_item(item=request, db=db, cls=Post)
 
 
 @router.post("/blog/tag/create/", status_code=201, response_model=CategoryOut, tags=["Blog Category"])
@@ -53,26 +44,38 @@ async def create_category(request: CreateCategory, db: AsyncSession = Depends(ge
     return await post_item(item=request, cls=Category, db=db)
 
 
+@router.get("/blog/{slug}/", response_model=PostOut, tags=["Blog Post"])
+async def get_post(slug: str, db: AsyncSession = Depends(get_session)):
+    """Endpoint to get single blog post."""
+    return await get_item(slug=slug, cls=Post, db=db)
+
+
 @router.get("/blog/tag/{slug}/", response_model=CategoryOut, tags=["Blog Category"])
 async def get_category(slug: str, db: AsyncSession = Depends(get_session)):
     """Endpoint to get single blog category."""
     return await get_item(slug=slug, cls=Category, db=db)
 
 
-@router.get("/blog/tags/", response_model=List[CategoryOut], tags=["Blog Category"])
-async def get_all_categories(db: AsyncSession = Depends(get_session)):
-    """Endpoint to get all blog posts or filtered blog posts."""
-    return await get_multiple_items(cls=Category, db=db)
+@router.put("/blog/{slug}/update/", status_code=200, response_model=PostOut, tags=["Blog Post"])
+async def update_post(request: UpdatePost, slug: str, db: AsyncSession = Depends(get_session)):
+    """Endpoint to update blog post."""
+    return await update_item(item=request, slug=slug, db=db, cls=Post)
 
 
 @router.put("/blog/tag/{slug}/update/", status_code=200, response_model=CategoryOut, tags=["Blog Category"])
 async def update_category(request: UpdateCategory, slug: str, db: AsyncSession = Depends(get_session)):
     """Endpoint to update blog category."""
     return await update_item(item=request, slug=slug, db=db, cls=Category)
+
+
+@router.delete("/blog/{slug}/delete/", status_code=410, response_model=PostOut, tags=["Blog Post"])
+async def delete_post(request: UpdatePost, slug: str, db: AsyncSession = Depends(get_session)):
+    """Endpoint to delete blog post."""
+    return await delete_item(item=request, slug=slug, cls=Post, db=db)
     
 
 @router.delete("/blog/tag/{slug}/delete/", status_code=410, tags=["Blog Category"])
-async def delete_category(request: UpdateCategory, slug: str, db: AsyncSession = Depends(get_session), tags=["Blog Category"]):
+async def delete_category(request: UpdateCategory, slug: str, db: AsyncSession = Depends(get_session)):
     """Endpoint to delete blog category."""
     return await delete_item(item=request, slug=slug, cls=Category, db=db)
     
