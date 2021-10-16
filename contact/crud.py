40ae5eca005da57d3d9812c169d4fc19adeafe48
drmacsika <drmacsika@gmail.com>
2021-10-16 19:34:23 +0100

    Add contact crud.

--- contact/crud.py
+++ contact/crud.py
@@ -2,42 +2,45 @@ from typing import List, Optional, Union
 
 from core.crud import BaseCRUD
 from fastapi import HTTPException
-from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from contact.models import Contact
 from contact.schemas import ContactCreate
 
-SLUGTYPE = Union[int, str]
+SLUGTYPE = Union[str, int]
 
 class ContactCRUD(BaseCRUD[Contact, ContactCreate, ContactCreate, SLUGTYPE]):
     
-    async def get(self, slug: SLUGTYPE, db: AsyncSession) -> Optional[Contact]:
+    async def get(self, *, slug: SLUGTYPE, db: AsyncSession) -> Optional[Contact]:
         try:
-            item = await super().get(slug, db)
-            if not item:
+            contact = await super().get(slug=slug, db=db)
+            if not contact:
                 raise HTTPException(status_code=404, detail="Contact not found.")
-            return item
-        except IntegrityError as ie:
-            raise ie.orig
-        except SQLAlchemyError as se:
-            raise se
-    
-    async def get_multiple(self, *, db: AsyncSession, offset: int = 0, limit: int = 0) -> List[Contact]:
-        try:
-            return await super().get_multiple(db, offset=offset, limit=limit)
+            return contact
         except IntegrityError as ie:
             raise ie.orig
         except SQLAlchemyError as se:
             raise se
         
-    async def create(
-        self, *, obj_in: ContactCreate,
-        db: AsyncSession, slug_field: SLUGTYPE = None
-        ) -> Contact:
+    async def get_multiple(
+        self, *,
+        db: AsyncSession,
+        offset: int = 0, 
+        limit: int = 100) -> List[Contact]:
         try:
-            return await super().create(obj_in, db, slug_field=slug_field)
+            contacts = await super().get_multiple(db=db, offset=offset, limit=limit)
+            if not contacts:
+                raise HTTPException(status_code=404, detail="Contact not found.")
+            return contacts
+        except IntegrityError as ie:
+            raise ie.orig
+        except SQLAlchemyError as se:
+            raise se
+    
+    async def create(self, *, obj_in: ContactCreate, db: AsyncSession) -> Contact:
+        try:
+            return await super().create(obj_in=obj_in, db=db)
         except IntegrityError as ie:
             raise ie.orig
         except SQLAlchemyError as se:
@@ -45,9 +48,7 @@ class ContactCRUD(BaseCRUD[Contact, ContactCreate, ContactCreate, SLUGTYPE]):
     
     async def delete(self, *, slug: SLUGTYPE, db: AsyncSession) -> Contact:
         try:
-            contact = self.get(slug=slug, db=db)
-            if not contact:
-                raise HTTPException(status_code=404, detail="Contact does not exist.")
+            await self.get(slug=slug, db=db)
             return await super().delete(slug=slug, db=db)
         except IntegrityError as ie:
             raise ie.orig
