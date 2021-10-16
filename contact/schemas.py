c8f835a764fa4d21c72ebb1928369b7e16636296
drmacsika <drmacsika@gmail.com>
2021-10-16 19:38:29 +0100

    Rename Contact to ContactOut.

--- contact/schemas.py
+++ contact/schemas.py
@@ -2,19 +2,26 @@ from datetime import datetime
 
 from pydantic import BaseModel, EmailStr
 
-"""Base fields for contact."""
+
 class ContactBase(BaseModel):
+    """
+    Base fields for contact.
+    """
     firstname: str
     lastname: str
     email: EmailStr
     message: str
 
-"""Fields for creating contacts."""
 class ContactCreate(ContactBase):
+    """
+    Fields for creating contacts.
+    """
     ...
 
-"""For the response."""
-class Contact(ContactBase):
+class ContactOut(ContactBase):
+    """
+    For Contact response.
+    """
     id: int
     status: bool = True
     created: datetime
