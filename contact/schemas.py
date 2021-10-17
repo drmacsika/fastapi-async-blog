ee0c96e23f7ddeb4978513265a68c5e7d7431ec3
drmacsika <drmacsika@gmail.com>
2021-10-17 01:16:12 +0100

    Add custom validations to required fields.

--- contact/schemas.py
+++ contact/schemas.py
@@ -1,7 +1,52 @@
 from datetime import datetime
+from re import compile, search
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
+
+
+def confirm_field(value: str) -> str:
+    """
+    Validation to prevent empty title field.
+    Called by the helperfunction below;
+    """
+    if not value:
+        raise ValueError("Please fill in missing field.")
+    return value
+
+def check_name_length(value: str) -> str:
+    """
+    Check the length of the name fields
+    """
+    if len(value) > 100:
+        raise ValueError("The provided name is too large.")
+    return value
+
+def check_special_character(value: str) -> str:
+    """
+    Check the presence of special characters.
+    """
+    special_characters = compile("[\" \"@_!#$%^&*()<>'\"?/\|}{~:]")
+    if special_characters.search(value) is not None:
+        raise ValueError("The provided name contains space or special character(s).")
+    return value
+
+def check_email_length(value: str) -> str:
+    """
+    Check the length of the email
+    """
+    if len(value) > 255:
+        raise ValueError("The provided email is too large.")
+    return value
+
+
+def check_message_length(value: str) -> str:
+    """
+    Check the length of the message
+    """
+    if len(value) > 1024:
+        raise ValueError("The message field is too large.")
+    return value
 
 
 class ContactBase(BaseModel):
@@ -12,6 +57,26 @@ class ContactBase(BaseModel):
     lastname: str
     email: EmailStr
     message: str
+    # Custom validation for first name field
+    _check_firstname = validator("firstname", allow_reuse=True)(confirm_field)
+    _check_fn_length = validator("firstname", allow_reuse=True)(check_name_length)
+    _check_fn_spec_chr = validator("firstname", allow_reuse=True)(check_special_character)
+
+    # Custom validation for last name field
+    _check_lastname = validator("lastname", allow_reuse=True)(confirm_field)
+    _check_ln_length = validator("lastname", allow_reuse=True)(check_name_length)
+    _check_ln_spec_chr = validator("lastname", allow_reuse=True)(check_special_character)
+    
+    # Custom validation for email field
+    _check_email = validator("email", allow_reuse=True)(confirm_field)
+    _check_email_length = validator("email", allow_reuse=True)(check_email_length)
+    
+    # Custom validation for message field
+    _check_message = validator("message", allow_reuse=True)(confirm_field)
+    _check_message_length = validator("message", allow_reuse=True)(check_message_length)
+    
+    
+    
 
 class ContactCreate(ContactBase):
     """
