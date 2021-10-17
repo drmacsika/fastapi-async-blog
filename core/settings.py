303a0f352c1c4d600e706d075e477476b7f056b3
drmacsika <drmacsika@gmail.com>
2021-10-17 23:24:03 +0100

    Enabled emails sending default settings.

--- core/settings.py
+++ core/settings.py
@@ -49,36 +49,36 @@ class Settings(BaseSettings):
     #         path=f"/{values.get('POSTGRES_DB') or ''}",
     #     )
     
-    # SMTP_TLS: bool = True
-    # SMTP_PORT: Optional[int] = None
-    # SMTP_HOST: Optional[str] = None
-    # SMTP_USER: Optional[str] = None
-    # SMTP_PASSWORD: Optional[str] = None
-    # EMAILS_FROM_EMAIL: Optional[EmailStr] = None
-    # EMAILS_FROM_NAME: Optional[str] = None
+    SMTP_TLS: bool = True
+    SMTP_PORT: Optional[int] = None
+    SMTP_HOST: Optional[str] = None
+    SMTP_USER: Optional[str] = None
+    SMTP_PASSWORD: Optional[str] = None
+    EMAILS_FROM_EMAIL: Optional[EmailStr] = None
+    EMAILS_FROM_NAME: Optional[str] = None
     
-    # @validator("EMAILS_FROM_NAME")
-    # def get_project_name(cls, v: Optional[str], values: Dict[str, Any]) -> str:
-    #     if not v:
-    #         return values["PROJECT_NAME"]
-    #     return v
+    @validator("EMAILS_FROM_NAME")
+    def get_project_name(cls, v: Optional[str], values: Dict[str, Any]) -> str:
+        if not v:
+            return values["PROJECT_NAME"]
+        return v
     
-    # EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
-    # EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
-    # EMAILS_ENABLED: bool = False
+    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
+    EMAIL_TEMPLATES_DIR: str = "/templates/build"
+    EMAILS_ENABLED: bool = False
     
-    # @validator("EMAILS_ENABLED", pre=True)
-    # def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
-    #     return bool(
-    #         values.get("SMTP_HOST")
-    #         and values.get("SMTP_PORT")
-    #         and values.get("EMAILS_FROM_EMAIL")
-    #     )
+    @validator("EMAILS_ENABLED", pre=True)
+    def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
+        return bool(
+            values.get("SMTP_HOST")
+            and values.get("SMTP_PORT")
+            and values.get("EMAILS_FROM_EMAIL")
+        )
         
-    # EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
-    # FIRST_SUPERUSER: EmailStr
-    # FIRST_SUPERUSER_PASSWORD: str
-    # USERS_OPEN_REGISTRATION: bool = False
+    EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
+    FIRST_SUPERUSER: EmailStr
+    FIRST_SUPERUSER_PASSWORD: str
+    USERS_OPEN_REGISTRATION: bool = False
     
     
     # SENTRY_DSN: Optional[HttpUrl] = None
