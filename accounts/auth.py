4110750a7fffc6bac5178f86d63b4074cb6cbd36
drmacsika <drmacsika@gmail.com>
2021-10-20 01:26:19 +0100

    Add endpoint for password recovery and password reset

--- accounts/auth.py
+++ accounts/auth.py
@@ -36,4 +36,49 @@ def test_token(current_user: User = Depends(get_current_user)) -> Any:
     return current_user
 
 
+@router.post("/password-recovery/{email}", response_model=Message)
+async def recover_password(email: str, db: AsyncSession = Depends(get_session)) -> Any:
+    """
+    Get a token to recover a lost or forgotten password.
+    """
+    u = await user.get(db=db, email=email)
+    if not u:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this username does not exist in the system.",
+        )
+    password_reset_token = generate_password_reset_token(email=email)
+    send_reset_password_email(
+        email_to=u.email, email=email, token=password_reset_token
+    )
+    return {"message": "Password recovery email sent"}
 
+
+@router.post("/reset-password/", response_model=Message)
+async def reset_password(
+    token: str = Body(...),
+    new_password: str = Body(...),
+    confirm_password: str = Body(...),
+    db: AsyncSession = Depends(get_session),
+) -> Any:
+    """
+    Reset a password after getting a password recovery token.
+    """
+    email = verify_password_reset_token(token)
+    if not email:
+        raise HTTPException(status_code=400, detail="Invalid token")
+    u = await user.get(db=db, email=email)
+    if new_password != confirm_password:
+        raise HTTPException(status_code=400, detail="Passwords do not match.")
+    if not u:
+        raise HTTPException(
+            status_code=404,
+            detail="The user with this username does not exist.",
+        )
+    elif not user.is_active(u):
+        raise HTTPException(status_code=400, detail="Inactive user")
+    hashed_password = get_password_hash(new_password)
+    u.password = hashed_password
+    db.add(user)
+    db.commit()
+    return {"message": "Password updated successfully"}
